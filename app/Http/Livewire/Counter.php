1b850cfd40b7191ac64596c2abce8cc1e01de62c
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-06-23 10:27:15 +0900

    basic inline component

---
+++ app/Http/Livewire/Counter.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $count = 0;
+
+    public function render()
+    {
+        return <<<'HTML'
+            <div class="grid place-content-center h-full">
+                <div class="counter rounded-full border-4 text-blue-700 border-blue-700 text-center text-5xl p-12">
+                    <span>{{ $count }}</span>
+                    <button wire:click="increment">+</button>
+                </div>
+            </div>
+        HTML;
+    }
+
+    public function increment()
+    {
+        $this->count++;
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,7 @@
 <html class="h-full">
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="/app/js"></script>
 
-    <div class="grid place-content-center h-full">
-        <div class="counter rounded-full border-4 text-blue-700 border-blue-700 text-center text-5xl p-12">
-            <span>0</span>
-            <button>+</button>
-        </div>
-    </div>
+    <livewire:counter></livewire:counter>
 
+    @livewireScripts
 </html>
