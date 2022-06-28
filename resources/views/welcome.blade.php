e6c8b3eb670123f5bd7982391e4605dbcb23c478
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-06-28 10:51:07 +0900

    triggering a server round-trip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,20 @@
     <script>
         document.querySelectorAll('[wire\\:snapshot]').forEach( el => {
             let snapshot = JSON.parse(el.getAttribute('wire:snapshot'));
-            console.log(snapshot);
+            el.addEventListener('click', e => {
+                if (!e.target.hasAttribute('wire:click')) return;
+
+                let method = e.target.getAttribute('wire:click');
+
+                fetch('/livewire', {
+                    method:  'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body:    JSON.stringify({
+                        snapshot,
+                        callMethod: method
+                    }),
+                })
+            })
         })
     </script>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome');
+Route::post(
+    '/livewire', function () {
+        return request()->all();
+    }
+);
 
 Blade::directive(
     'livewire', function ($expression) {
