c8bd1746190d12ef2bcc9e5cc7a12ac4e12c42d8
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-07-05 09:53:45 +0900

    building a TODO component

---
+++ app/Http/Livewire/Todos.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Livewire;
+
+/**
+ * Demo todo class
+ */
+class Todos
+{
+    public $draft;
+    public $todos = [
+        'One todo',
+        'Two todo',
+    ];
+
+    public function addTodo()
+    {
+        $this->todos[] = $this->draft;
+        $this->draft = '';
+    }
+
+
+    /**
+     * Template
+     *
+     * @return heredoc
+     */
+    public function render()
+    {
+        return <<<'HTML'
+            <div class="grid h-full gap-4 todos place-content-center">
+                <div class="space-x-2">
+                    <input wire:model="draft" type="text" class="px-4 py-2 border border-black rounded-full">
+                    <button wire:click="addTodo" class="px-4 py-2 text-white bg-black rounded-full">Add Todos</button>
+                </div>
+
+                <ul class="mt-4 text-lg font-bold list-disc list-inside">
+                    @foreach($todos as $todo)
+                        <li>{{ $todo }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        HTML;
+    }
+}
